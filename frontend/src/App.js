5e75d45fd53224fae2988c9cff49ce077624d964
Himanshu Mishra <mishrahimanshu070@gmail.com>
2023-06-19 18:47:25 +0530

    Downgraded react version and header done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import Header from "./components/Header"
+import Body from "./components/Body";
+import Footer from "./components/Footer";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <Body/>
+      <Footer/>
     </div>
   );
 }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./css/Footer.css";
+
+function Footer() {
+  return (
+    <div className="link">
+      <span className="footer-text">
+        <a href="https://www.highradius.com/privacy-policy/" target="_">Privacy Policy</a>{" "}
+        | C icon 2023 HighRadius Corporation. All rights reserved.
+      </span>
+    </div>
+  );
+}
+export default Footer;
