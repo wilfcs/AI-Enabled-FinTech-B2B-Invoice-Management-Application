8f88aecceca9f49cda83aa9cfd6dd3e19f3d4746
Himanshu Mishra <mishrahimanshu070@gmail.com>
2023-06-19 20:18:10 +0530

    Table added, header and footer done

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './css/Footer.css'
+
+const Footer = () => {
+  return (
+    <div className="link">
+      <span className="footer-text">
+        <a href="https://www.highradius.com/privacy-policy/" target='_'>Privacy Policy</a>{" "}
+        | C 2023 HighRadius Corporation. All rights reserved.
+      </span>
+    </div>
+  );
+}
+
+export default Footer
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './css/Header.css'
+import abcLogo from '../images/abclogo.svg'
+import hrclogo from '../images/hrclogo.svg'
+
+const Header = () => {
+  return (
+    <div className="header-body">
+      <div class="left">
+        <img src={hrclogo} alt="hrclogo" />
+      </div>
+      <div class="middle">
+        <img src={abcLogo} alt="abcLogo" />
+      </div>
+    </div>
+  );
+}
+
+export default Header
